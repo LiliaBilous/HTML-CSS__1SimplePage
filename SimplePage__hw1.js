c1af1378fa3224f8b27a8abc821119d68fa5f51d
Liliia <91496734+LiliaBilous@users.noreply.github.com>
2024-07-05 21:35:15 +0300

    Update SimplePage__hw1.js

--- SimplePage__hw1.js
+++ SimplePage__hw1.js
@@ -2,10 +2,10 @@ window.addEventListener("load", windowLoad);
 
 function windowLoad() {
   const conteiner = document.querySelector(".main-conteiner");
-  //get saved theme
+ 
   const saveUserTheme = localStorage.getItem("user-theme");
-  //робота з системними налаштуваннями
   let userTheme;
+  
   if (window.matchMedia) {
     userTheme = window.matchMedia("(prefers-color-scheme: dark)").matches
       ? "dark"
@@ -16,12 +16,12 @@ function windowLoad() {
     .addEventListener("change", (e) => {
       !saveUserTheme ? changeTheme() : null;
     });
-  //додаємо оьробник події на кнопку зміни теми
+ 
   const toggle = document.querySelector(".header__theme-button");
   toggle.addEventListener("click", () => {
     changeTheme(true);
   });
-  //зберігаємо уподобання відносно вибраної теми
+ 
   function setThemeClass() {
     if (saveUserTheme) {
       conteiner.classList.add(saveUserTheme);
